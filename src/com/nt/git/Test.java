45ec56e45e172108498a0e4688b76dcca3e4a2c8
Umesh <Umesh@DESKTOP-H3MT9SH>
2019-06-23 00:42:02 +0530

    This is my first commit

---
+++ src/com/nt/git/Test.java
@@ -0,0 +1,20 @@
+package com.nt.git;
+
+public class Test {
+  int a=10;
+  int b=10;
+  int c;
+	void add(){
+		c=a+b;
+		System.out.println("add"+c);
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
